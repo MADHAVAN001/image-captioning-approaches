f13fa83f7102be89320bd45e569ef23abcc21ea7
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-19 22:39:01 +0000

    Fix inception lstm bug

--- models/Inception_LSTM_Googlecc.py
+++ models/Inception_LSTM_Googlecc.py
@@ -54,20 +54,17 @@ if __name__ == "__main__":
         os.path.join(cfg["workspace"]["directory"], cfg["dataset"]["name"], "word_dictionary.txt")
     )
 
-    image_input = Input(shape=(2048,))
-    im1 = Dropout(0.5)(image_input)
-    im2 = Dense(256, activation='relu')(im1)
+    img_input = Input(shape=(2048,))
+    img_enc = Dense(300, activation="relu")(img_input)
+    images = RepeatVector(MAX_LEN)(img_enc)
 
+    # Text input
     text_input = Input(shape=(MAX_LEN,))
-    sent1 = Embedding(vocab_size, EMBEDDING_DIM, mask_zero=True)(text_input)
-    sent2 = Dropout(0.5)(sent1)
-    sent3 = LSTM(256)(sent2)
-
-    decoder1 = Add()([im2, sent3])
-    decoder2 = Dense(256, activation='relu')(decoder1)
-    pred = Dense(vocab_size, activation='softmax')(decoder2)
-
-    model = Model(inputs=[image_input, text_input], outputs=pred)
+    embedding = Embedding(vocab_size, EMBEDDING_DIM, input_length=MAX_LEN)(text_input)
+    x = Concatenate()([images, embedding])
+    y = Bidirectional(LSTM(256, return_sequences=False))(x)
+    pred = Dense(vocab_size, activation='softmax')(y)
+    model = Model(inputs=[img_input, text_input], outputs=pred)
     model.compile(loss='categorical_crossentropy', optimizer="RMSProp", metrics=['accuracy'])
 
     model.summary()
